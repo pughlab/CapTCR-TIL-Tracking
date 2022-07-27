5f5e12a81ebccc2d3e44b26f29155164af592ab1
Cameron Kerr <51450639+CameronKerr@users.noreply.github.com>
2022-07-27 18:30:13 -0400

    Update Top10Comparison.R

--- R/Calculation-Functions/Top10Comparison.R
+++ R/Calculation-Functions/Top10Comparison.R
@@ -12,32 +12,36 @@
 # @param file_samplekeys: file name of the sample keys 
 
 Top104WComp <- function(patient, sampcohort, chain, clnefrc, 
-                   dir_clones, dir_samplekeys, file_samplekeys){
-
-    # Loads in patient data
-    Load_data(patient, sampcohort, chain, clnefrc, dir_clones, dir_samplekeys, file_samplekeys)
-    
-    # Loading patient sample order
-    samporder <- eval(as.name(paste(patient, sampcohort, sep="")))
-    
-    # Create Top104W dataframe out of the top 10 clones in the 4 week sample
-    Top104W <- FW_data[1:10,]
-    Top104W$TIL_cloneFraction <- replicate(10, 0)
-    Top104W$Base_cloneFraction <- replicate(10, 0)
-    
-    # Fill in the TIL_cloneFraction and Base_cloneFraction with the clone fraction of the corresponding 4 week clone
-    for(i in 1:10){
-        if(length(TIL_data$cloneFraction[which(TIL_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])])>0){
-            Top104W$TIL_cloneFraction[i] <- TIL_data$cloneFraction[which(TIL_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])]
-        }
-        if(length(Base_data$cloneFraction[which(Base_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])])>0){
-            Top104W$Base_cloneFraction[i] <- Base_data$cloneFraction[which(Base_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])]
-        }
+                        dir_clones, dir_samplekeys, file_samplekeys){
+  
+  # Loads in patient data
+  Load_data(patient, sampcohort, chain, clnefrc, dir_clones, dir_samplekeys, file_samplekeys)
+  
+  # Loading patient sample order
+  samporder <- eval(as.name(paste(patient, sampcohort, sep="")))
+  
+  # Create Top104W dataframe out of the top 10 clones in the 4 week sample
+  Top104W <- FW_data[1:10,]
+  if(patient=="TLML_1_"){
+    Top104W <- CDR3_fraction[which(CDR3_fraction$filename==TLML_1_gDNA[3]),][1:10,]
+  }
+  Top104W$TIL_cloneFraction <- replicate(10, 0)
+  Top104W$Base_cloneFraction <- replicate(10, 0)
+  
+  # Fill in the TIL_cloneFraction and Base_cloneFraction with the clone fraction of the corresponding 4 week clone
+  for(i in 1:10){
+    if(length(TIL_data$cloneFraction[which(TIL_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])])>0){
+      Top104W$TIL_cloneFraction[i] <- TIL_data$cloneFraction[which(TIL_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])]
     }
-    
-    # Calculates and prints average cloneFraction for the TIL and Base Data for the top 10 4 week clones
-    TIL_cloneFraction <- sum(Top104W$TIL_cloneFraction)/10
-    Base_cloneFraction <- sum(Top104W$Base_cloneFraction)/10
-    print(TIL_cloneFraction)
-    print(Base_cloneFraction)
+    if(length(Base_data$cloneFraction[which(Base_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])])>0){
+      Top104W$Base_cloneFraction[i] <- Base_data$cloneFraction[which(Base_data$aaSeqCDR3 == Top104W$aaSeqCDR3[i])]
+    }
+  }
+  
+  # Calculates and prints average cloneFraction for the TIL and Base Data for the top 10 4 week clones
+  TIL_cloneFraction <- sum(Top104W$TIL_cloneFraction)/10
+  Base_cloneFraction <- sum(Top104W$Base_cloneFraction)/10
+  print(TIL_cloneFraction)
+  print(Base_cloneFraction)
+  print(sum(Top104W$cloneFraction)/10)
 }
