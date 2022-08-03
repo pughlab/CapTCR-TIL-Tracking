a450cdcfffdc2040997c86cb02a35461ff747dcf
Cameron Kerr <51450639+CameronKerr@users.noreply.github.com>
2022-08-03 18:17:20 -0400

    Update PlotAlignment.R

--- R/Plot-Functions/PlotAlignment.R
+++ R/Plot-Functions/PlotAlignment.R
@@ -43,21 +43,25 @@ alignment_fig <- function(patients, sampcohort, chain, clnefrc, dir_clones, dir_
         if(figure=="clonetrack"){
             ClonetrackPlot(width_df$patients[i], sampcohort, chain, clnefrc, dir_clones, 
                            dir_samplekeys, file_samplekeys, primary)
-        }
+          myp1 <- myp + theme(axis.text.x=element_blank(), axis.title.y = element_blank()) + scale_y_continuous(labels=function(x)paste0(x*100, "%"), sec.axis = sec_axis(~.*500))
+          
+          }
         if(figure=="diversity"){
             DivPlot(width_df$patients[i], sampcohort, chain, clnefrc, dir_clones, 
                            dir_samplekeys, file_samplekeys, primary, 500)
-        }
+            myp1 <- myp + theme(axis.text.x=element_blank(), axis.title.y = element_blank())
+          }
         if(figure=="relative"){
             RelPlot(width_df$patients[i], sampcohort, chain, clnefrc, dir_clones, 
                            dir_samplekeys, file_samplekeys, primary)
+            myp1 <- myp + theme(axis.text.x=element_blank(), axis.title.y = element_blank())
         }
         if(figure=="rel_div"){
             Overlay_RelDiv(width_df$patients[i], sampcohort, chain, clnefrc, dir_clones, 
                            dir_samplekeys, file_samplekeys, primary, 500)
+            myp1 <- myp + theme(axis.text.x=element_blank(), axis.title.y = element_blank())
         }
-        #myp1 <- myp + theme(axis.text.x = element_blank(), axis.text.y = element_blank(), axis.title.y = element_blank())
-        myp1 <- myp
+
         assign(width_df$patients[i], myp1)
         pltlst[[i]] <- eval(as.name(width_df$patients[i]))
     }
