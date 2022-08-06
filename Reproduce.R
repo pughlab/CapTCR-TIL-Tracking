63f8d6978d604ce7fc363a0f2e7c74a4a70a1100
Cameron Kerr <51450639+CameronKerr@users.noreply.github.com>
2022-08-06 12:45:57 -0400

    Update Reproduce.R

--- Reproduce.R
+++ Reproduce.R
@@ -1,7 +1,7 @@
 # Produces serperate files from mixcr output file
 
 # Loading packages and scripts
-GitHub_path <- getwd()
+GitHub_path <- paste(getwd(), "/", sep="")
 dir_main <- paste(GitHub_path, "data/", sep="")
 file_samplekeys <- "TLML_samples_keys.xlsx"
 
